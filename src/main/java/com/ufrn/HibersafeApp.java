57ab6df6780ff3715f381746108d44caf850d8a1
Carlos Antonio <carlosantoniooln@gmail.com>
2021-12-17 00:08:29 -0300

    Correção

--- src/main/java/com/ufrn/HibersafeApp.java
+++ src/main/java/com/ufrn/HibersafeApp.java
@@ -24,7 +24,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableJpaAuditing
-@SpringBootApplication(scanBasePackages="com.ufrn.api")
+@SpringBootApplication(scanBasePackages="com.ufrn")
 @Api(value = "Hibersafe API")
 @EnableSwagger2
 @EntityScan("com.ufrn.api")
