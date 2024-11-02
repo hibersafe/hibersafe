be38401af0a4cc01a04b057f0e3d9e3f8a8670ed
Carlos Antonio <carlosantoniooln@gmail.com>
2024-11-02 14:15:36 -0300

    Exceções da versão 6.6

--- src/main/java/com/ufrn/utils/Exceptions.java
+++ src/main/java/com/ufrn/utils/Exceptions.java
@@ -2,16 +2,48 @@ package com.ufrn.utils;
 
 public class Exceptions {
 	
-	public static final String HIBERNATE_EXCEPTIONS = "AnnotationException" + "|AssertionFailure" + "|CallbackException" + "|DuplicateMappingException"
-            + "|HibernateError" + "|HibernateException" + "|InstantiationException" + "|InvalidMappingException" + "|JDBCException" + "|LazyInitializationException"
-            + "|MappingException" + "|MappingNotFoundException" + "|NonUniqueObjectException" + "|NonUniqueResultException" + "|ObjectDeletedException"
-            + "|ObjectNotFoundException" + "|OptimisticLockException" + "|PersistentObjectException" + "|PessimisticLockException"
-            + "|PropertyAccessException" + "|PropertyNotFoundException" + "|PropertySetterAccessException" + "|PropertyValueException" + "|QueryException"
-            + "|QueryParameterException" + "|QueryTimeoutException" + "|ResourceClosedException" + "|SessionException" + "|StaleObjectStateException"
-            + "|StaleStateException" + "|TransactionException" + "|TransientObjectException" + "|TransientPropertyValueException" + "|TypeMismatchException" 
-            + "|UnknownEntityTypeException" + "|UnknownProfileException" + "|UnresolvableObjectException" + "|UnsupportedLockAttemptException" 
-            + "|WrongClassException" + "|ConstraintViolationException" + "|DataException" + "|GenericJDBCException" + "|JDBCConnectionException" 
-            + "|LockAcquisitionException" + "|LockTimeoutException" + "|SQLGrammarException";
+	public static final String HIBERNATE_EXCEPTIONS = 
+	        "AnnotationException" + "|AssertionFailure" + "|CallbackException" + 
+	        "|DuplicateMappingException" + "|EntityFilterException" + "|FetchNotFoundException" + "|HibernateError" + 
+	        "|HibernateException" + "|InstantiationException" + "|InvalidMappingException" + 
+	        "|JDBCException" + 
+	        "|LazyInitializationException" + 
+	        "|MappingException" + 
+	        "|NonUniqueObjectException" + 
+	        "|NonUniqueResultException" + 
+	        "|ObjectDeletedException" + 
+	        "|ObjectNotFoundException" + 
+	        "|PersistentObjectException" + 
+	        "|PessimisticLockException" + 
+	        "|PropertyAccessException" + 
+	        "|PropertyNotFoundException" + 
+	        "|PropertySetterAccessException" + 
+	        "|PropertyValueException" + 
+	        "|QueryException" + 
+	        "|QueryParameterException" + 
+	        "|QueryTimeoutException" + 
+	        "|ResourceClosedException" + 
+	        "|SessionException" + 
+	        "|StaleObjectStateException" + 
+	        "|StaleStateException" + 
+	        "|TransactionException" + 
+	        "|TransactionManagementException" + 
+	        "|TransientObjectException" + 
+	        "|TransientPropertyValueException" + 
+	        "|TypeMismatchException" + 
+	        "|UnknownEntityTypeException" + 
+	        "|UnknownFilterException" + 
+	        "|UnknownProfileException" + 
+	        "|UnresolvableObjectException" + 
+	        "|UnsupportedLockAttemptException" + 
+	        "|WrongClassException" + 
+	        "|ConstraintViolationException" + 
+	        "|DataException" + 
+	        "|GenericJDBCException" + 
+	        "|JDBCConnectionException" + 
+	        "|LockAcquisitionException" + 
+	        "|LockTimeoutException" + 
+	        "|SQLGrammarException";
 	
 	public Exceptions() {
 	}
